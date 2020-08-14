a11b01c37286eb36c246f29cb02245c37c15f5d1
Joel <joelgroomer@gmail.com>
2020-08-13 19:55:11 -0700

    Eating cookies complete, with cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,39 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+def eating_cookies(n, cache=[]):
+    # Your code here
+    # print(f'n = {n}')
+    # base case
+    if n == 0:
+        return 1
+
+    if n < 0:
+        return 0
+
+    # set up cache
+    if len(cache) == 0:
+        for _ in range(n+1):
+            cache.append(0)
+        print(f'cache length {len(cache)} for input {n}')
+
+    # check cache
+    # print(f'{n} / {len(cache)}')
+    if cache[n] != 0:
+        return cache[n]
+
+    # make 3 recursive calls
+    val = eating_cookies(n-1, cache) + eating_cookies(n-2,
+                                                      cache) + eating_cookies(n-3, cache)
+    cache[n] = val
+    return val
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 2
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
